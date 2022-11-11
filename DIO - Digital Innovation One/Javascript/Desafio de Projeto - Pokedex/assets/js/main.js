7517f0a7aeb6c4e73d1079e790e89485a907f614
estevaobarbieri <estevao193@gmail.com>
2022-11-11 11:19:03 -0300

    Realizado 80% do Desafio de Projeto JS

--- DIO - Digital Innovation One/Javascript/Desafio de Projeto - Pokedex/assets/js/main.js
+++ DIO - Digital Innovation One/Javascript/Desafio de Projeto - Pokedex/assets/js/main.js
@@ -1,12 +1,30 @@
 
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(response){
-    console.log(response)
+function convertPokemonToLI(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+
+            <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+
+        </div>
+
+    </li>
+`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLI).join('')
+
 })
-
-const x = 10 +10 
-console.log(x)
