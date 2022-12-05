035b46dc7976d2fd43486625a5d9ced5a9b29a24
estevaobarbieri <estevao193@gmail.com>
2022-12-05 14:45:58 -0300

    Desafio Calculadora Concluido

---
+++ DIO - Digital Innovation One/Javascript/React/Desafios/desafio-calculadora/src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #A9A9A9;
+    background-color: #D3D3D3;
+    color: #000000;
+    font-size: 24px;
+    font-weight: 700;
+    flex:1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
